554c618be90af349e3035e59464b8e0945820431
Aravinda.S.V <aravisv@gmail.com>
2022-07-15 23:26:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 const title = document.querySelector("#main-heading").style.color = "white";
+
+const listItems = document.querySelectorAll('.list-items');
+for( let i=0; i<listItems.length; i++){
+	listItems[i].style.fontSize = '1em';
+}
+
+const ul = document.querySelector('ul');
+//createElement
+const li = document.createElement('li');
+
+//add to DOM
+ul.append(li);
+
+//const firstListItem = document.querySelector('li');
+//console.log(firstListItem.innerHTML);
+//console.log(firstListItem.innerText);
+//console.log(firstListItem.textContent);
+
+li.innerText = "Thor : Love and Thunder";
+
+li.setAttribute('class', 'list-items');
